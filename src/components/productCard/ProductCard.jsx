546e89db3521c7a3c76a56f8e0c4f66e0e1c4af5
Lucas-ez <lucasezequiel237@gmail.com>
2023-01-19 20:40:22 -0300

    Add product page

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function ProductCard({product}) {
-  console.log(product);
   return (
     <div>
       <Link to={`/product?id=${product.id}`}>{product.title}</Link>
--- src/pages/product/ProductPage.jsx
+++ src/pages/product/ProductPage.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
+import './productPage.css'
 
 export default function ProductPage({allProducts}) {
   const {search} = useLocation()
   const query = new URLSearchParams(search)
   const id = query.get('id')
 
+  
   const product = allProducts.filter(p => p.id === Number(id))[0]
-  console.log(product, id);
+  console.log(product);
   return (
     <div className='ProductPage'>
-      {product.title}
-      <img src={product.image} alt="" />
+      <div className="container">
+        <div className="top-section">
+          <img src={product.image} alt={product.title} />
+          <div className='right-section'>
+            <span>{`more ${product.category}`}</span>
+            <span>{product.title}</span>
+            <span>{product.rating.rate} ({product.rating.count})</span>
+            <span>$ {product.price}</span>
+          </div>
+        </div>
+        <div className="bottom-section">
+          {product.description}
+        </div>
+      </div>
     </div>
   )
 }
