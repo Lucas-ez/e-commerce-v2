99623dda02be25a08d244d10061d0877639ba9fc
Lucas-ez <lucasezequiel237@gmail.com>
2023-01-19 21:09:28 -0300

    Add product card

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './productCard.css'
 
 export default function ProductCard({product}) {
+
+  const {id, image, title, price} = {...product}
+
   return (
-    <div>
-      <Link to={`/product?id=${product.id}`}>{product.title}</Link>
-    </div>
+    <Link to={`/product?id=${product.id}`} className='ProductCard'>
+      <img src={product.image} alt={product.title} />
+      <h6 className='title'>
+        {product.title}
+      </h6>
+      <div className='bottom-section'>
+        <h2 className='price'>{"$ " + product.price}</h2>
+      </div>
+    </Link>
   )
 }
