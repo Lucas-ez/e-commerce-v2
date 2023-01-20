fbcdcb6a3a1db9dbe47555231405029616c56bb4
Lucas-ez <lucasezequiel237@gmail.com>
2023-01-19 21:09:52 -0300

    Add home page, list products

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ProductCard } from './../../components'
+import './homePage.css'
 
 export default function HomePage({allProducts}) {
   return (
     <div className='HomePage'>
-      {
-        (allProducts.length === 0) ?
-        <h2>Loading...</h2>
-        :
-        allProducts.map(product => <ProductCard key={product.id} product={product}/>)
-      }
+      <div className='container'>
+        {
+          (allProducts.length === 0) ?
+          <h2>Loading...</h2>
+          :
+          allProducts.map(product => <ProductCard key={product.id} product={product}/>)
+        }
+      </div>
     </div>
   )
 }
